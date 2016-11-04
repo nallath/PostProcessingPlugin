b23f4048f364ca077a6059801fac3b0b170adcac
Jaime van Kessel <nallath@gmail.com>
2016-11-04 11:39:00 +0100

    Codestyle fixes
    CURA-2607

--- scripts/PauseAtHeight.py
+++ scripts/PauseAtHeight.py
@@ -1,4 +1,6 @@
 from ..Script import Script
+
+
 class PauseAtHeight(Script):
     def __init__(self):
         super().__init__()
@@ -87,14 +89,13 @@ class PauseAtHeight(Script):
                     current_z = self.getValue(line, 'Z')
                     x = self.getValue(line, 'X', x)
                     y = self.getValue(line, 'Y', y)
-                    if current_z != None:
+                    if current_z is not None:
                         if current_z >= pause_z:
-
                             index = data.index(layer)
-                            prevLayer = data[index-1]
-                            prevLines = prevLayer.split("\n")
+                            previous_layer = data[index - 1]
+                            previous_lines = previous_layer.split("\n")
                             current_e = 0.
-                            for prevLine in reversed(prevLines):
+                            for prevLine in reversed(previous_lines):
                                 current_e = self.getValue(prevLine, 'E', -1)
                                 if current_e >= 0:
                                     break
@@ -104,53 +105,53 @@ class PauseAtHeight(Script):
                             prepend_gcode += ";script: PauseAtHeight.py\n"
                             prepend_gcode += ";current z: %f \n" % (current_z)
 
-                            #Retraction
+                            # Retraction
                             prepend_gcode += "M83\n"
                             if retraction_amount != 0:
-                                prepend_gcode += "G1 E-%f F6000\n" % (retraction_amount)
+                                prepend_gcode += "G1 E-%f F6000\n" % retraction_amount
 
-                            #Move the head away
+                            # Move the head away
                             prepend_gcode += "G1 Z%f F300\n" % (current_z + 1)
                             prepend_gcode += "G1 X%f Y%f F9000\n" % (park_x, park_y)
                             if current_z < 15:
                                 prepend_gcode += "G1 Z15 F300\n"
 
-                            #Disable the E steppers
+                            # Disable the E steppers
                             prepend_gcode += "M84 E0\n"
-                            #Wait till the user continues printing
+                            # Wait till the user continues printing
                             prepend_gcode += "M0 ;Do the actual pause\n"
 
-                            #Push the filament back,
+                            # Push the filament back
                             if retraction_amount != 0:
-                                prepend_gcode += "G1 E%f F6000\n" % (retraction_amount)
+                                prepend_gcode += "G1 E%f F6000\n" % retraction_amount
 
                             # Optionally extrude material
                             if extrude_amount != 0:
-                                prepend_gcode += "G1 E%f F200\n" % (extrude_amount)
+                                prepend_gcode += "G1 E%f F200\n" % extrude_amount
 
-                            # and retract again, the properly primes the nozzle when changing filament.
+                            # Retract again, the properly primes the nozzle when changing filament.
                             if retraction_amount != 0:
-                                prepend_gcode += "G1 E-%f F6000\n" % (retraction_amount)
+                                prepend_gcode += "G1 E-%f F6000\n" % retraction_amount
 
-                            #Move the head back
+                            # Move the head back
                             prepend_gcode += "G1 Z%f F300\n" % (current_z + 1)
-                            prepend_gcode +="G1 X%f Y%f F9000\n" % (x, y)
+                            prepend_gcode += "G1 X%f Y%f F9000\n" % (x, y)
                             if retraction_amount != 0:
-                                prepend_gcode +="G1 E%f F6000\n" % (retraction_amount)
-                            prepend_gcode +="G1 F9000\n"
-                            prepend_gcode +="M82\n"
+                                prepend_gcode += "G1 E%f F6000\n" % retraction_amount
+                            prepend_gcode += "G1 F9000\n"
+                            prepend_gcode += "M82\n"
 
-                            # reset extrude value to pre pause value
-                            prepend_gcode +="G92 E%f\n" % (current_e)
+                            # Reset extrude value to pre pause value
+                            prepend_gcode += "G92 E%f\n" % current_e
 
                             layer = prepend_gcode + layer
 
-                            # include a number of previous layers
+                            # Include a number of previous layers
                             for i in range(1, redo_layers + 1):
-                                prevLayer = data[index-i]
-                                layer = prevLayer + layer
+                                previous_layer = data[index - i]
+                                layer = previous_layer + layer
 
-                            data[index] = layer #Override the data of this layer with the modified data
+                            data[index] = layer  # Override the data of this layer with the modified data
                             return data
                         break
         return data
